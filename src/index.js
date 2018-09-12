91e129544e9127f1e8d7c2d162968a4a8bfe8cb8
Kangrui Zhao <kzhao11@hawk.iit.edu>
2018-09-12 11:25:52 -0700

    make it better3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './App';
 import reducers from './reducers';
-import registerServiceWorker from './registerServiceWorker';
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
